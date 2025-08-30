7c05e3adad124d2d0e190cdd9688f83cfe06d96a
lymansix <zhaowangliu68@gmail.com>
2025-08-30 13:23:13 +0800

    delete home info

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,71 +1,17 @@
 <template>
   <div class="dashboard-editor-container">
-    <panel-group @handleSetLineChartData="handleSetLineChartData" />
-
-    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
-      <line-chart :chart-data="lineChartData" />
-    </el-row>
-
-    <el-row :gutter="32">
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <raddar-chart />
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <pie-chart />
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <bar-chart />
-        </div>
-      </el-col>
-    </el-row>
-
-
+    <p>WELCOME</p>
   </div>
 </template>
 
 <script>
-import BarChart from './dashboard/BarChart'
-import LineChart from './dashboard/LineChart'
-import PanelGroup from './dashboard/PanelGroup'
-import PieChart from './dashboard/PieChart'
-import RaddarChart from './dashboard/RaddarChart'
-
-const lineChartData = {
-  newVisitis: {
-    expectedData: [100, 120, 161, 134, 105, 160, 165],
-    actualData: [120, 82, 91, 154, 162, 140, 145]
-  },
-  messages: {
-    expectedData: [200, 192, 120, 144, 160, 130, 140],
-    actualData: [180, 160, 151, 106, 145, 150, 130]
-  },
-  purchases: {
-    expectedData: [80, 100, 121, 104, 105, 90, 100],
-    actualData: [120, 90, 100, 138, 142, 130, 130]
-  },
-  shoppings: {
-    expectedData: [130, 140, 141, 142, 145, 150, 160],
-    actualData: [120, 82, 91, 154, 162, 140, 130]
-  }
-}
 
 export default {
   name: 'Index',
   components: {
-    PanelGroup,
-    LineChart,
-    RaddarChart,
-    PieChart,
-    BarChart
   },
   data() {
     return {
-      lineChartData: lineChartData.newVisitis
     }
   },
   methods: {
@@ -77,31 +23,39 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-.dashboard-promote .el-row{
+.dashboard-promote .el-row {
   margin-bottom: 20px;
   font-size: 14px;
 }
-.dashboard-promote .el-row .jz{
+
+.dashboard-promote .el-row .jz {
   height: 500px;
 }
-.dashboard-promote .el-row .kj{
+
+.dashboard-promote .el-row .kj {
   height: 300px;
 }
-.dashboard-promote .el-row .xx{
+
+.dashboard-promote .el-row .xx {
   height: 200px;
 }
 
-.git-res{
+.git-res {
   margin-top: 20px;
 }
-.git-res .el-link{
+
+.git-res .el-link {
   margin-right: 30px;
 }
 
-ul,li{ padding:0;margin:0;list-style:none}
+ul,
+li {
+  padding: 0;
+  margin: 0;
+  list-style: none
+}
 
-.product li{
+.product li {
   margin-bottom: 20px;
   float: left;
   width: 150px;
